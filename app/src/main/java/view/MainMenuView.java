615bbd9097954194f7afb1c5a7df390e6fcb1b17
DESKTOP-TJJRH84\whdgn <whdgns5059@gmail.com>
2023-09-05 14:07:02 +0900

    MainMenuView의 화면 이동부분을 추가 하였습니다. 인터페이스의 수정이 있었습니다.

--- app/src/main/java/view/MainMenuView.java
+++ app/src/main/java/view/MainMenuView.java
@@ -22,9 +22,20 @@ public class MainMenuView implements ConsoleView{
         return moveNextMenu();
     }
 
+
+    //이 메소드는 Factory 패턴을 이용해 개선 할 수 있을것 같습니다.
     private ConsoleView moveNextMenu() {
         if(status == 1){
-            return new MainMenuView();
+            // return 여행 입력 화면 인스턴스
+        }
+        if(status == 2){
+            // return 여정 입력 화면 인스턴스
+        }
+        if(status == 3){
+            // return 여행 조회 화면 인스턴스
+        }
+        if(status == 4){
+            // return 여정 조회 화면 인스턴스
         }
         if(status == 5){
             return new ProgramExit();
