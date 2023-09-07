c716ff827180a1e2c67396fccb47639aaaf6a647
DESKTOP-TJJRH84\whdgn <whdgns5059@gmail.com>
2023-09-07 10:27:21 +0900

    프로그램 반복 및 종료 기능 추가

--- app/src/main/java/view/MainMenuView.java
+++ app/src/main/java/view/MainMenuView.java
@@ -5,10 +5,11 @@ import java.util.Scanner;
 public class MainMenuView implements ConsoleView {
 
     Scanner sc;
-    int status;
+    boolean isContinue;
 
     public MainMenuView() {
         this.sc = new Scanner(System.in);
+        isContinue = true;
     }
 
 
@@ -18,29 +19,39 @@ public class MainMenuView implements ConsoleView {
         printDivider();
         printMenuList();
         printDivider();
-        inputMenuNumber();
         moveNextMenu();
     }
 
 
     //이 메소드에 대해 개선 필요성이 있는거 같습니다.
-    private ConsoleView moveNextMenu() {
+    private void moveNextMenu() {
+        System.out.print("시작 할 메뉴 번호를 입력하세요 : ");
+        int status = sc.nextInt();
+
         if (status == 1) {
             ConsoleView cv = new TripInputView();
             cv.print();
+            return;
         }
         if (status == 2) {
-            // return 여정 입력 화면 인스턴스
+            System.out.println("여정입력 기능 구현 필요");
+            return;
         }
         if (status == 3) {
-            // return 여행 조회 화면 인스턴스
+            System.out.println("여행조회 기능 구현 필요");
+            return;
         }
         if (status == 4) {
-            // return 여정 조회 화면 인스턴스
+            System.out.println("여정조회 기능 구현 필요");
+            return;
         }
         if (status == 5) {
-            System.out.println("\n프로그램을 종료합니다.");
-            System.exit(0);
+            isContinue = false;
+            System.out.println();
+            printDivider();
+            System.out.println("프로그램을 종료합니다.");
+            printDivider();
+            return;
         }
 
         throw new IllegalArgumentException("잘 못 된 입력 입니다.");
@@ -55,10 +66,6 @@ public class MainMenuView implements ConsoleView {
         System.out.println("여행기록(1), 여정기록(2), 여행조회(3), 여정조회(4), 종료(5)");
     }
 
-    private void inputMenuNumber() {
-        System.out.print("시작 할 메뉴 번호를 입력하세요 : ");
-        status = sc.nextInt();
-    }
 
     private void printDivider() {
         StringBuilder sb = new StringBuilder();
@@ -68,7 +75,7 @@ public class MainMenuView implements ConsoleView {
         System.out.println(sb.toString());
     }
 
-    public boolean isEnd() {
-        return false;
+    public boolean isContinue() {
+        return isContinue;
     }
 }
